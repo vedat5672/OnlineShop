72ff801f779ef790625217970775a64dc3689220
vedat5672 <63197396+vedat5672@users.noreply.github.com>
2021-12-24 02:41:59 +0300

    SESSİON 24.12.2021 02:38
    session olusturuldu
    startupta session configure ettim
    uygulamamda sepet ve siparis controller
    ve viewları olusturdum
    product modelimi guncellestirm
    ck editor kullandım
    datatables.net sitesinden aldıgım javascript ve css dosyalarıyla
    admin panelimi design atttim

---
+++ OnlineShop/Areas/Customer/Controllers/OrderController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Data;
+using OnlineShop.Models;
+using OnlineShop.Utility;
+
+namespace OnlineShop.Areas.Customer.Controllers
+{
+    [Area("customer")]
+    [Route("customer")]
+    [Route("customer/order")]
+    public class OrderController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public OrderController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+      
+        //GET Checkout actioin method
+        [Route("checkout")]
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+      
+        //POST Checkout action method
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("checkout")]
+        public async Task<IActionResult> Checkout(Order anOrder)
+        {
+            List<Products> products = HttpContext.Session.Get<List<Products>>("products");
+            if(products!=null)
+            {
+                foreach (var product in products)
+                {
+                    OrderDetails orderDetails=new OrderDetails();
+                    orderDetails.PorductId = product.Id;
+                    anOrder.OrderDetails.Add(orderDetails);
+                }
+            }
+
+            anOrder.OrderNo = GetOrderNo();
+            _db.Orders.Add(anOrder);
+            await _db.SaveChangesAsync();
+            HttpContext.Session.Set("products", new List<Products>());
+            return View();
+        }
+
+        [Route("getorderno")]
+        public string GetOrderNo()
+        {
+            int rowCount = _db.Orders.ToList().Count()+1;
+            return rowCount.ToString("000");
+        }
+    }
+}
